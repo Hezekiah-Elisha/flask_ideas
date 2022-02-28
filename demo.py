baa8a3d9871aa774d16c2ea84bb0cb64b10add1b
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2022-03-01 01:58:19 +0300

    adding new routes

--- demo.py
+++ demo.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/football', methods = ['GET'])
+def football():
+    return render_template('football.html')
+
 if __name__ == '__main__':
     app.run(port = 7000, debug = True)
