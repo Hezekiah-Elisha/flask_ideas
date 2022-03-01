a459c37ccc72f75e46cf400f16bdeb79b02f6e8d
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2022-03-01 21:43:30 +0300

    Processing client data

--- demo.py
+++ demo.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['GET'])
+@app.route('/', methods = ['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html', message = 'Welcome to the index page')
+    else:
+        username = request.form['username']
+        password = request.form['password']
+        if username == 'Hezekiah' and password == 'elisha':
+            return render_template('football.html', message = 'Log in successful')
+        else:
+            error_message = 'Hint: He curses a lot'
+            return render_template('index.html', message = error_message)
 
 @app.route('/football', methods = ['GET'])
 def football():
