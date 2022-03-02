42fc21fa2baed94ad1716be45c01829f492bcee2
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2022-03-02 23:23:53 +0300

    User login AND session management

--- demo.py
+++ demo.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for, g
 import model
 
 app = Flask(__name__)
+app.secret_key = 'plutofuture'
+
+username = ''
+user = model.check_users()
 
 @app.route('/', methods = ['GET', 'POST'])
 def home():
+    if 'username' in session:
+        g.user = session['username']
+        return render_template('football.html')
+    return render_template('homepage.html', message = 'log in to the page or sign up')
+
+
+    """
     if request.method == 'GET':
         return render_template('index.html', message = 'Welcome to the index page')
     else:
@@ -18,6 +29,17 @@ def home():
         else:
             error_message = 'Hint: He curses a lot'
             return render_template('index.html', message = error_message)
+    """
+
+@app.route('/login', methods = ['GET','POST'])
+def login():
+    if request.method == 'POST':
+        session.pop('username', None)
+        areyouuser = rrequest.form['username']
+        pwd = model.check_pw(areyouuser)
+        if request.form['password'] == pwd:
+            session['username'] = request.form['username']
+            return redirect(url_for('home'))
 
 @app.route('/football', methods = ['GET'])
 def football():
